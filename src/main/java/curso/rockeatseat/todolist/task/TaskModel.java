994c722d7caee3392d81560912a44bc0728bb06c
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2023-10-11 17:05:09 -0300

    New dependency bcrypt, new entity for tasks.

---
+++ src/main/java/curso/rockeatseat/todolist/task/TaskModel.java
@@ -0,0 +1,66 @@
+package curso.rockeatseat.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "tb_tasks")
+public class TaskModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private UUID idUser;
+    private String description;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(LocalDateTime startAt) {
+        this.startAt = startAt;
+    }
+
+    public LocalDateTime getEndAt() {
+        return endAt;
+    }
+
+    public void setEndAt(LocalDateTime endAt) {
+        this.endAt = endAt;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
--- src/main/java/curso/rockeatseat/todolist/user/UserController.java
+++ src/main/java/curso/rockeatseat/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package curso.rockeatseat.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,10 @@ public class UserController {
         if(this.userRepository.findByUsername(userModel.getUsername()) != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var hashedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(hashedPassword);
+
         var userCreated = userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
