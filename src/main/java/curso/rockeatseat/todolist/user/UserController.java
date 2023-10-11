1c144b37a7280394c6baa62db691c1dba1fcf955
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2023-10-11 03:02:27 -0300

    Added dependency H2 Database, changed userModel to be recognize as entity, created interface user repository.

--- src/main/java/curso/rockeatseat/todolist/user/UserController.java
+++ src/main/java/curso/rockeatseat/todolist/user/UserController.java
@@ -1,13 +1,22 @@
 package curso.rockeatseat.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());
+    public ResponseEntity<Object> create(@RequestBody UserModel userModel) {
+        if(this.userRepository.findByUsername(userModel.getUsername()) != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        var userCreated = userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
--- src/main/java/curso/rockeatseat/todolist/user/UserModel.java
+++ src/main/java/curso/rockeatseat/todolist/user/UserModel.java
@@ -1,9 +1,30 @@
 package curso.rockeatseat.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "tb_users")
 public class UserModel {
 
-    private String username, name, password;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    @Column(name = "username", unique = true)
+    private String username;
+    private String  name, password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public UUID getUid() {
+        return id;
+    }
     public String getUsername() {
         return username;
     }
@@ -26,5 +47,9 @@ public class UserModel {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 }
