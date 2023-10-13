5e3d5b4ac13a13d9d6955bbcd083e4fe8b3ad3c2
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2023-10-13 00:04:11 -0300

    Now taskController validate inserted datetime.

--- src/main/java/curso/rockeatseat/todolist/task/TaskController.java
+++ src/main/java/curso/rockeatseat/todolist/task/TaskController.java
@@ -2,11 +2,14 @@ package curso.rockeatseat.todolist.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -17,8 +20,15 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create (@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity<Object> create (@RequestBody TaskModel taskModel, HttpServletRequest request) {
         taskModel.setIdUser((UUID) request.getAttribute("idUser"));
-        return this.taskRepository.save(taskModel);
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt()) ) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / término deve ser maior do que a data atual.");
+        }
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor do que a data de término.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(this.taskRepository.save(taskModel));
     }
 }
