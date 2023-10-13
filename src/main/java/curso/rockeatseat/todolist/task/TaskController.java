b8bc6bef6ca9d94a82a8eed81de18bbec2ce08ac
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2023-10-13 00:24:21 -0300

    New route to list all tasks from user.

--- src/main/java/curso/rockeatseat/todolist/task/TaskController.java
+++ src/main/java/curso/rockeatseat/todolist/task/TaskController.java
@@ -4,12 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -30,5 +28,11 @@ public class TaskController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve ser menor do que a data de término.");
         }
         return ResponseEntity.status(HttpStatus.OK).body(this.taskRepository.save(taskModel));
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        return this.taskRepository.findAllByIdUser((UUID) idUser);
     }
 }
